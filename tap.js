76ffedfd5f75b4e49f6ffc3fa275247d039f525e
tkoset <cmckr88@gmail.com>
2022-04-29 22:56:23 +0300

    Create tap.js

---
+++ tap.js
@@ -0,0 +1,459 @@
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test1&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+   
+     <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test2&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test3&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test4&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test5&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test6&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test7&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test8&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test9&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test10&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test11&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test12&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test13&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test14&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test15&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test16&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test17&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+  <script language='javascript' type='text/javascript'>
+       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var sound = 1,
+  ala = [&#39;figure-logo&#39;, &#39;figure-logo-return&#39;];
+$(&#39;.test18&#39;).click(function() {
+  $(this).removeClass(ala[sound % 2])
+    .addClass(ala[++sound % 2]);
+  sound = sound % 2;
+});
+      
+}
+   </script>
+
+  <script language='javascript' type='text/javascript'>
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var pound = 1,
+  bla = [&#39;figure-art&#39;, &#39;figure-art-return&#39;];
+$(&#39;.rest1&#39;).click(function() {
+  $(this).removeClass(bla[pound % 2])
+    .addClass(bla[++pound % 2]);
+  pound = pound % 2;
+});
+      
+}   
+       </script>
+	   
+	     <script language='javascript' type='text/javascript'>
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var pound = 1,
+  bla = [&#39;figure-art&#39;, &#39;figure-art-return&#39;];
+$(&#39;.rest2&#39;).click(function() {
+  $(this).removeClass(bla[pound % 2])
+    .addClass(bla[++pound % 2]);
+  pound = pound % 2;
+});
+      
+}   
+       </script>
+	   
+	     <script language='javascript' type='text/javascript'>
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var pound = 1,
+  bla = [&#39;figure-art&#39;, &#39;figure-art-return&#39;];
+$(&#39;.rest3&#39;).click(function() {
+  $(this).removeClass(bla[pound % 2])
+    .addClass(bla[++pound % 2]);
+  pound = pound % 2;
+});
+      
+}   
+       </script>
+	   
+	   
+
+	     <script language='javascript' type='text/javascript'>
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var pound = 1,
+  bla = [&#39;figure-art&#39;, &#39;figure-art-return&#39;];
+$(&#39;.rest4&#39;).click(function() {
+  $(this).removeClass(bla[pound % 2])
+    .addClass(bla[++pound % 2]);
+  pound = pound % 2;
+});
+      
+}   
+       </script>
+	   
+	   
+	     <script language='javascript' type='text/javascript'>
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var pound = 1,
+  bla = [&#39;figure-art&#39;, &#39;figure-art-return&#39;];
+$(&#39;.rest5&#39;).click(function() {
+  $(this).removeClass(bla[pound % 2])
+    .addClass(bla[++pound % 2]);
+  pound = pound % 2;
+});
+      
+}   
+       </script>
+	   
+	   
+	     <script language='javascript' type='text/javascript'>
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var pound = 1,
+  bla = [&#39;figure-art&#39;, &#39;figure-art-return&#39;];
+$(&#39;.rest6&#39;).click(function() {
+  $(this).removeClass(bla[pound % 2])
+    .addClass(bla[++pound % 2]);
+  pound = pound % 2;
+});
+      
+}   
+       </script>
+	   
+	   
+	     <script language='javascript' type='text/javascript'>
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var pound = 1,
+  bla = [&#39;figure-art&#39;, &#39;figure-art-return&#39;];
+$(&#39;.rest7&#39;).click(function() {
+  $(this).removeClass(bla[pound % 2])
+    .addClass(bla[++pound % 2]);
+  pound = pound % 2;
+});
+      
+}   
+       </script>
+	   
+	   
+	     <script language='javascript' type='text/javascript'>
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var pound = 1,
+  bla = [&#39;figure-art&#39;, &#39;figure-art-return&#39;];
+$(&#39;.rest8&#39;).click(function() {
+  $(this).removeClass(bla[pound % 2])
+    .addClass(bla[++pound % 2]);
+  pound = pound % 2;
+});
+      
+}   
+       </script>
+	   
+	   
+	     <script language='javascript' type='text/javascript'>
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var pound = 1,
+  bla = [&#39;figure-art&#39;, &#39;figure-art-return&#39;];
+$(&#39;.rest9&#39;).click(function() {
+  $(this).removeClass(bla[pound % 2])
+    .addClass(bla[++pound % 2]);
+  pound = pound % 2;
+});
+      
+}   
+       </script>
+	   
+	     
+  <script language='javascript' type='text/javascript'>
+          if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var hound = 1,
+  cla = [&#39;figure-poster&#39;, &#39;figure-poster-return&#39;];
+$(&#39;.fest1&#39;).click(function() {
+  $(this).removeClass(cla[hound % 2])
+    .addClass(cla[++hound % 2]);
+  hound = hound % 2;
+});
+      
+} 
+</script>
+
+  
+  <script language='javascript' type='text/javascript'>
+          if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var hound = 1,
+  cla = [&#39;figure-poster&#39;, &#39;figure-poster-return&#39;];
+$(&#39;.fest2&#39;).click(function() {
+  $(this).removeClass(cla[hound % 2])
+    .addClass(cla[++hound % 2]);
+  hound = hound % 2;
+});
+      
+} 
+</script>
+
+  
+  <script language='javascript' type='text/javascript'>
+          if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var hound = 1,
+  cla = [&#39;figure-poster&#39;, &#39;figure-poster-return&#39;];
+$(&#39;.fest3&#39;).click(function() {
+  $(this).removeClass(cla[hound % 2])
+    .addClass(cla[++hound % 2]);
+  hound = hound % 2;
+});
+      
+} 
+</script>
+
+  
+  <script language='javascript' type='text/javascript'>
+          if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var hound = 1,
+  cla = [&#39;figure-poster&#39;, &#39;figure-poster-return&#39;];
+$(&#39;.fest4&#39;).click(function() {
+  $(this).removeClass(cla[hound % 2])
+    .addClass(cla[++hound % 2]);
+  hound = hound % 2;
+});
+      
+} 
+</script>
+
+  
+  <script language='javascript' type='text/javascript'>
+          if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var hound = 1,
+  cla = [&#39;figure-poster&#39;, &#39;figure-poster-return&#39;];
+$(&#39;.fest5&#39;).click(function() {
+  $(this).removeClass(cla[hound % 2])
+    .addClass(cla[++hound % 2]);
+  hound = hound % 2;
+});
+      
+} 
+</script>
+
+  
+  <script language='javascript' type='text/javascript'>
+          if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) { 
+
+var hound = 1,
+  cla = [&#39;figure-poster&#39;, &#39;figure-poster-return&#39;];
+$(&#39;.fest6&#39;).click(function() {
+  $(this).removeClass(cla[hound % 2])
+    .addClass(cla[++hound % 2]);
+  hound = hound % 2;
+});
+      
+} 
+</script>
+
